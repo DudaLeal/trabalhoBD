f99c8601c2d4811cd22a2bbe568994ea67dec5fa
DudaLeal <duda.leal1999@gmail.com>
2023-11-27 22:54:04 -0300

    Criação de pagina web para analise de deputados

--- BD/BD-Web.py
+++ BD/BD-Web.py
@@ -5,9 +5,9 @@ import pymysql.cursors
 # Creando a conexão com o Bando de Dados -> importante já ter ele salvo.
 host = 'localhost'
 user = 'root'
-password = 'password'
+password = '040899'
 #Definindo o Banco de Dados a ser Usado
-database = 'Deputados'
+database = 'deputados'
 # Estabelecendoa conexão
 connection = pymysql.connect(host=host, user=user, password=password, database=database,cursorclass=pymysql.cursors.DictCursor)
 # Fechando Conexão
@@ -16,22 +16,61 @@ connection = pymysql.connect(host=host, user=user, password=password, database=d
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/deputados')
 def deputados():
     with connection.cursor() as cursor:
         query_args = []
         if request.args.get("search"):
-            sql = "SELECT * FROM `deputados` WHERE LOWER(`nome`) LIKE LOWER(%s) ORDER BY `nome` ASC"
+            sql = """
+                    SELECT d.*, l.dataInicio AS dataInicioLegislatura, l.dataFim AS dataFimLegislatura, p.nome AS nomePartido
+                    FROM `deputados` d
+                    INNER JOIN `Legislatura` l ON d.idLegislaturaInicial = l.idLegislatura
+                    INNER JOIN `Partido` p ON d.idPartido = p.idPartido
+                    WHERE LOWER(d.nome) LIKE LOWER(%s)
+                    ORDER BY d.nome ASC
+                """
             search = "%{}%".format(request.args["search"])
             query_args = [search]
         else:
-            sql = "SELECT * FROM `deputados` ORDER BY `nome` ASC"
+            sql = """
+                    SELECT d.*, l.dataInicio AS dataInicioLegislatura, l.dataFim AS dataFimLegislatura, p.nome AS nomePartido
+                    FROM `deputados` d
+                    INNER JOIN `Legislatura` l ON d.idLegislaturaInicial = l.idLegislatura
+                    INNER JOIN `Partido` p ON d.idPartido = p.idPartido
+                    ORDER BY `nome` ASC
+                """
         cursor.execute(sql, query_args)
         deputados = cursor.fetchall()
 
     return render_template("deputados.html", deputados=deputados, search=request.args.get("search"))
 
+@app.route('/g1')
+def genero():
+    with connection.cursor() as cursor:
+        sql = """
+                SELECT d.sexo, COUNT(*) AS total
+                FROM `FrenteParlamentar` f
+                INNER JOIN `Deputados` d ON f.idDeputadoCoordenador = d.idDeputado
+                GROUP BY d.sexo
+            """
+        cursor.execute(sql)
+        deputados = cursor.fetchall()
 
+    return render_template("genero.html", deputados=deputados)
+
+@app.route('/g2')
+def partidos():
+    with connection.cursor() as cursor:
+        sql = """
+            SELECT p.nome AS nomePartido, COUNT(d.idDeputado) AS total
+            FROM Partido p
+            INNER JOIN Deputados d ON p.idPartido = d.idPartido
+            GROUP BY p.nome
+        """
+        cursor.execute(sql)
+        partidos = cursor.fetchall()
+
+    return render_template("partido.html", partidos=partidos)
 
 if __name__ == '__main__':
     app.run(debug = True)
---
+++ BD/queries/insertPartido.sql
@@ -0,0 +1,18 @@
+-- Inserção de dados na tabela Partido
+INSERT INTO Partido (idPartido, sigla, nome)
+VALUES
+    (36898, 'AVANTE', 'Avante'),
+    (37905, 'CIDADANIA', 'Cidadania'),
+    (36899, 'MDB', 'Movimento Democrático Brasileiro'),
+    (37901, 'NOVO', 'Partido Novo'),
+    (37907, 'PATRIOTA', 'Patriota'),
+    (36779, 'PCdoB', 'Partido Comunista do Brasil'),
+    (36786, 'PDT', 'Partido Democrático Trabalhista'),
+    (37906, 'PL', 'Partido Liberal'),
+    (36896, 'PODE', 'Podemos'),
+    (37903, 'PP', 'Progressistas'),
+    (36832, 'PSB', 'Partido Socialista Brasileiro'),
+    (36834, 'PSD', 'Partido Social Democrático'),
+    (36835, 'PSDB', 'Partido da Social Democracia Brasileira'),
+    (36839, 'PSOL', 'Partido Socialismo e Liberdade'),
+    (36844, 'PT', 'Partido dos Trabalhadores');
